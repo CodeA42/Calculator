6479ae7c03d6c2b2503f940a53919bdfdfaea0e8
Aleksandar Velichkov <sashko98@gmail.com>
2023-07-20 01:18:27 +0300

    Add some packages

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  console.log('Hello world!')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{title: 'Welcome'}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen}/>
+      </Stack.Navigator>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
+
+const HomeScreen = ({navigation}: any) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+
+const ProfileScreen = ({navigation, route}: any) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
