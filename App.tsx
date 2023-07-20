61488ed2e481650d0d33da6c343e5ccca2afdc04
alexander.velichkov <alexander.velichkov@pytek.bg>
2023-07-20 13:22:02 +0300

    Add type

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import * as React from 'react'
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as React from "react";
 
 const Stack = createNativeStackNavigator();
 
+type RootStackParamList = {
+  Home: undefined;
+  Profile: { name: string };
+};
+
 export default function App() {
-  console.log('Hello world!')
+  console.log("Hello world!");
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-        name='Home'
-        component={HomeScreen}
-        options={{title: 'Welcome'}}/>
-        <Stack.Screen name="Profile" component={ProfileScreen}/>
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
@@ -25,26 +34,28 @@ export default function App() {
   );
 }
 
-const HomeScreen = ({navigation}: any) => {
+const HomeScreen = ({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}) => {
   return (
     <Button
       title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Profile', {name: 'Jane'})
-      }
+      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
     />
   );
 };
 
-const ProfileScreen = ({navigation, route}: any) => {
+const ProfileScreen = ({ navigation, route }: any) => {
   return <Text>This is {route.params.name}'s profile</Text>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
